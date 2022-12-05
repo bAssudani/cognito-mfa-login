90b84d67501372ea5c683adf8079188741410571
LeonelAstors <118379205+LeonelAstors@users.noreply.github.com>
2022-12-05 14:09:28 -0500

    Update config.jsx

--- src/Assets/config.jsx
+++ src/Assets/config.jsx
@@ -1,7 +1,7 @@
 
 const UserPoolData = {
-    userPoolId : "",
-    clientId:""
+    userPoolId : "us-east-1_UeY1GcPF8",
+    clientId:"2o03j7krom48d0tivudkihj435"
 }
 
 export default UserPoolData;
